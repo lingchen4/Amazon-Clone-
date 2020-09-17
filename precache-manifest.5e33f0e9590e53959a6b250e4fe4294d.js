49339879c16cdb7a309f9fad88007ab2aa7cdfde
Ling Chen <ling.duke@gmail.com>
2020-09-17 01:10:28 -0400

    Updates

---
+++ precache-manifest.5e33f0e9590e53959a6b250e4fe4294d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3c74868315a0f7ddf11d96c4a3a4bbf5",
+    "url": "/lingchen4/Amazon-Clone-/index.html"
+  },
+  {
+    "revision": "0482eaa1975038682a42",
+    "url": "/lingchen4/Amazon-Clone-/static/css/main.56b5a28f.chunk.css"
+  },
+  {
+    "revision": "ee84c8b4209a6ac15e6e",
+    "url": "/lingchen4/Amazon-Clone-/static/js/2.bb328dfa.chunk.js"
+  },
+  {
+    "revision": "5f8be6edcb631ea3c834b8459dc2c264",
+    "url": "/lingchen4/Amazon-Clone-/static/js/2.bb328dfa.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0482eaa1975038682a42",
+    "url": "/lingchen4/Amazon-Clone-/static/js/main.6c707105.chunk.js"
+  },
+  {
+    "revision": "3118078c9c5789f469ac",
+    "url": "/lingchen4/Amazon-Clone-/static/js/runtime-main.03c9a23b.js"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/lingchen4/Amazon-Clone-/precache-manifest.5e33f0e9590e53959a6b250e4fe4294d.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/lingchen4/Amazon-Clone-/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+});
